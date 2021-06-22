7feaf1740760631bf04f73401b8b05239ba2310a
Syed Huzaifa Ali <80565640+syedhuzaifaali660@users.noreply.github.com>
2021-06-22 21:55:16 +0500

    Add files via upload

---
+++ AutoTyping.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoTyping : MonoBehaviour
+{
+
+    public string story;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        this.GetComponent<Text>().text = "";
+        StartCoroutine(PlayText());
+    }
+    IEnumerator PlayText()
+    {
+        foreach (char c in story)
+        {
+            this.GetComponent<Text>().text += c.ToString();
+            yield return new WaitForSecondsRealtime(0.05f);
+        }
+    }
+
+
+}
